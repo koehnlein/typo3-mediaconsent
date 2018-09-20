75e066b3dbd6051f7ce9314f872018b33ea4dd1d
Christoph Roth <christoph.roth@lka.ekvw.de>
2018-09-20 16:30:33 +0200

    basic config as content element

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,8 +1,42 @@
 <?php
+// Adds the content element to the "Type" dropdown
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+   array(
+      'LLL:EXT:mediaconsent/Resources/Private/Language/locallang.xlf:mediaconsent_cns.wizard.title',
+      'mediaconsent_cns',
+      'EXT:mediaconsent/Resources/Public/Icons/Extension.svg'
+   ),
+   'CType',
+   'mediaconsent'
+);
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'ArbkomEKvW.Mediaconsent',
-    'Pi1',
-    'Consent Content Element',
-    'EXT:mediaconsent/Resources/Public/Icons/Extension.svg'
+// Configure the default backend fields for the content element
+$GLOBALS['TCA']['tt_content']['types']['mediaconsent_cns'] = array(
+   'showitem' => '
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+         --palette--;;general,
+         --palette--;;headers,
+         bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+         --palette--;;frames,
+         --palette--;;appearanceLinks,
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+         --palette--;;language,
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+         --palette--;;hidden,
+         --palette--;;access,
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+         categories,
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+         rowDescription,
+      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+   ',
+   'columnsOverrides' => [
+      'bodytext' => [
+         'config' => [
+            'enableRichtext' => false,
+            'richtextConfiguration' => 'default'
+         ]
+      ]
+   ]
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function () {
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'ArbkomEKvW.Mediaconsent',
-            'Pi1',
-            [
-                'Mediaconsent' => 'list',
-            ],
-            // non-cacheable actions
-            [
-                'Mediaconsent' => 'list',
-            ]
-        );
-    }
+// Register content element icons
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+$iconRegistry->registerIcon(
+    'tx_mediaconsent_cns',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    [
+        'source' => 'EXT:mediaconsent/Resources/Public/Icons/Extension.svg',
+    ]
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mediaconsent/Configuration/PageTS/PageTSConfig.t3s">'
 );
