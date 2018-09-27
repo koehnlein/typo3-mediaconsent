edfebc53fc2879ed441d38b024a9771ed94b75a9
Christoph Roth <christoph.roth@lka.ekvw.de>
2018-09-27 15:52:53 +0200

    made content element uncached, begin JavaScript development

--- Classes/DataProcessing/MediaconsentCnsProcessor.php
+++ Classes/DataProcessing/MediaconsentCnsProcessor.php
@@ -23,6 +23,8 @@ use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
 class MediaconsentCnsProcessor implements DataProcessorInterface
 {
 
+  private $smcProviders;
+
    /**
     * Process data for the content element Mediaconsent Cns
     * Data of the content element is already in $processedData['data']!
@@ -39,32 +41,42 @@ class MediaconsentCnsProcessor implements DataProcessorInterface
       array $processorConfiguration,
       array $processedData
    ) {
+      $this->initProviders($processorConfiguration['cnProviders']);
 
       // read allowed content sources from session
       $allowedSources = $GLOBALS['TSFE']->fe_user->getKey('ses', 'allowFromSource');
       if (! is_array($allowedSources) ) $allowedSources = [];
 
-      $smcProvider = $processedData['data']['mediaconsent_smcprovider'];
+      $smcProviderNum = $processedData['data']['mediaconsent_smcprovider'];
 
       // do this only if a content element uid is arriving in mediaconsent_item:
       // because this also means user has clicked to allow the content
       if ( intval($_GET['mediaconsent_item']) > 0 ) { 
         // check if content provider exists in session, save if not
-        if (! in_array($smcProvider, $allowedSources))  {
-          array_push($allowedSources, $smcProvider);
+        if (! in_array($smcProviderNum, $allowedSources))  {
+          array_push($allowedSources, $smcProviderNum);
           $GLOBALS['TSFE']->fe_user->setKey('ses', 'allowFromSource', $allowedSources);
         }
       }
 
-      if (in_array($smcProvider, $allowedSources)) {
+      if (in_array($smcProviderNum, $allowedSources)) {
         $processedData['wrapperActive'] = 0;
       } else {
         $processedData['wrapperActive'] = 1;
       }
 
       $processedData['reloadPageType'] = $processorConfiguration['reloadPageType'];
+      $processedData['smcProvider'] = $this->getSmcProvider($smcProviderNum);
 
       return $processedData;
    }
+
+  private function initProviders($smcProvidersString) {
+    $this->smcProviders = explode(',', $smcProvidersString);
+  }
+
+  private function getSmcProvider($num) {
+    return $this->smcProviders[$num - 1];
+  }
    
 }
---
+++ Resources/Public/js/tx_mediaconsent.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('.mediaconsent_wrapper').click(function(event) {
+        console.log($(this).attr('data-mcid'));
+    });
+});
+
