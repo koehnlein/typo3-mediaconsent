38ba1755bade54b5bf7a3e550d89223a172a1ed1
Christoph Roth <christoph.roth@lka.ekvw.de>
2018-11-22 15:53:13 +0100

    Change in JavaScript: Now reloads all Elements of same content provider

--- Resources/Public/js/tx_mediaconsent.js
+++ Resources/Public/js/tx_mediaconsent.js
@@ -1,11 +1,25 @@
+var mcNsp = (function namespace() {
+
+    var reloadItem = function(item) {
+       $.get(item.attr('data-mcuri'), function(data) {
+            item.off('click');
+            item.html(data);
+       }, 'html');
+    };
+
+    return {
+        reloadItem: reloadItem
+    };
+}());
+
 $(function() {
     $('.mediaconsent_wrapper').on('click', function(event) {
-        var reloadUrl = $(this).attr('data-mcuri');
-        var wrapper = $(this);
-        $.get(reloadUrl, function(data) {
-            wrapper.off('click');
-            wrapper.html(data);
-        }, 'html');
+
+        // Walk over list of items matching the type in attr('data-mctype')
+        var scpType = $(this).attr('data-mctype');
+        $('.mediaconsent_wrapper[data-mctype=' + scpType + ']').each(function() {
+            mcNsp.reloadItem($(this));
+        });
+
     });
 });
-
