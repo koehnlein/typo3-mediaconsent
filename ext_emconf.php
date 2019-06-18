3b57a23bd35cc70a3ffed35d90be26f78a10556d
Christoph Roth <christoph.roth@lka.ekvw.de>
2019-06-18 14:09:01 +0200

    Header field of element is now rendered

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'christoph.roth@lka.ekvw.de',
     'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '0.1.5',
+    'version' => '0.1.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.8-8.7.99',
         ]
     ]
 ];
