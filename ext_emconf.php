85a0daa90f09a360310c791130e5e0163cd09c96
Albrecht Köhnlein <ak@koehnlein.eu>
2020-06-08 20:38:15 +0200

    [UPDATE] Set dependencies to TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.7',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.8-9.5.99',
+            'typo3' => '8.7.8-10.4.99',
         ]
     ]
 ];
